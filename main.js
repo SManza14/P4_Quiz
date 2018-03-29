2c5db08c58ecc91e48c84c7ce7c25b6a5880e586
Sergio <s.manzanerop@alumnos.upm.es>
2018-03-29 12:00:09 +0200

    creación del servidor

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ const cmds = require('./cmds');
 //const model = require('./model');
 const {log, biglog, errorlog, colorize} = require('./out');
 
+const net = require('net');
+
+net.createServer(socket => {
+
+  console.log("Se ha conectado un cliente desde " + socket.remoteAddress)
 
 // Mensaje inicial
 biglog('CORE Quiz', 'green');
@@ -14,11 +19,11 @@ const rl = readline.createInterface({
   output: process.stdout,
   prompt: colorize("quiz > ", "blue"),
   completer: (line) => {
-  	const completions = 'h help add delete edit list test show p play credits q quit'.split(' ');
-  	const hits = completions.filter((c) => c.startsWith(line));
-  	// show all completions if none found
-  	return [hits.length ? hits : completions, line];
-	}
+    const completions = 'h help add delete edit list test show p play credits q quit'.split(' ');
+    const hits = completions.filter((c) => c.startsWith(line));
+    // show all completions if none found
+    return [hits.length ? hits : completions, line];
+  }
 });
 
 rl.prompt();
@@ -26,13 +31,13 @@ rl.prompt();
 rl
 .on('line', (line) => {
 
-	let args = line.split(" ");
-	let cmd = args[0].toLowerCase().trim();
+  let args = line.split(" ");
+  let cmd = args[0].toLowerCase().trim();
 
   switch (cmd) {
-  	case '':
-  	rl.prompt();
-  		break;
+    case '':
+    rl.prompt();
+      break;
 
     case 'h':
     case 'help':
@@ -41,42 +46,42 @@ rl
 
      case 'q':
      case 'quit':
-      	cmds.quitCmd(rl);
-  		break;
+        cmds.quitCmd(rl);
+      break;
 
-  	case 'add':
-  		cmds.addCmd(rl);
-  		break;
+    case 'add':
+      cmds.addCmd(rl);
+      break;
 
-  	case 'list':
-  		cmds.listCmd(rl);
-  		break;
+    case 'list':
+      cmds.listCmd(rl);
+      break;
 
-  	case 'show':
-  		cmds.showCmd(rl, args[1]);
-  		break;
+    case 'show':
+      cmds.showCmd(rl, args[1]);
+      break;
 
-  	case 'test':
-  		cmds.testCmd(rl, args[1]);
-  		break;
+    case 'test':
+      cmds.testCmd(rl, args[1]);
+      break;
 
-  	case 'play':
-  	case 'p':
-  		cmds.playCmd(rl);
-  		break;
+    case 'play':
+    case 'p':
+      cmds.playCmd(rl);
+      break;
 
-  	case 'delete':
-  		cmds.deleteCmd(rl, args[1]);
-  		break;
+    case 'delete':
+      cmds.deleteCmd(rl, args[1]);
+      break;
 
-  	case 'edit':
-  		cmds.editCmd(rl, args[1]);
-  		break;
+    case 'edit':
+      cmds.editCmd(rl, args[1]);
+      break;
 
-  	case 'credits':
-  		cmds.creditsCmd(rl);
-  		 break;
-  		 
+    case 'credits':
+      cmds.creditsCmd(rl);
+       break;
+       
     default:
       log(`  Comando desconocido: '${colorize(cmd, 'red')}'`);
       log(`  Pruebe ${colorize('help', 'green')} para ver todos los comandos disponibles`);
@@ -88,3 +93,7 @@ rl
   log('Adios!');
   process.exit(0);
 });
+
+})
+.listen(3030);
+
